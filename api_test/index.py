460ea0566254f222c91119ecacea92456475f212
Arch <111789310+Arch881010@users.noreply.github.com>
2024-02-29 14:48:42 -0600

    Fix?: Workflow

--- api_test/index.py
+++ api_test/index.py
@@ -4,6 +4,7 @@ a = os.getcwd()
 if(("\\youtube-downloader\\api_test") in a):
     ""
 else:
+    print(a)
     raise ValueError("Wrong Directory - You are not in the correct directory. Please run this script from the api_test directory.")
 
 print("Running clean.py tests...")
