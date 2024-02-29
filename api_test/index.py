93a78f7b2b8d66d49c9138bb8b453c724e5f9699
Arch <111789310+Arch881010@users.noreply.github.com>
2024-02-29 14:54:48 -0600

    Bypass: Dir lock

--- api_test/index.py
+++ api_test/index.py
@@ -5,7 +5,7 @@ if(("\\youtube-downloader\\api_test") in a):
     ""
 else:
     print(a)
-    raise ValueError("Wrong Directory - You are not in the correct directory. Please run this script from the api_test directory.")
+    #raise ValueError("Wrong Directory - You are not in the correct directory. Please run this script from the api_test directory.")
 
 print("Running clean.py tests...")
 import clean
